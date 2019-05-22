27bafe15f3798c8636d2f709a96c302c7510caf5
Alec Keeter <aleckeeter94@gmail.com>
2019-05-21 22:31:44 -0400

    Fixes to pics

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,7 +1,8 @@
 class PicsController < ApplicationController
-  before_action :find_pic, only: [:show, :edit, :update, :destroy]
+  before_action :find_pic, only: [ :show, :edit, :update, :destroy]
 
   def index
+    @pics = Pic.all.order("created_at DESC")
   end
 
   def show
@@ -21,14 +22,30 @@ class PicsController < ApplicationController
     end
   end
 
-  private
+  def edit
+  end
+
+  def update
+    if @pic.update(pic_params)
+      redirect_to @pic, notice: "Congrats! Pic was updated"
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @pic.destroy
+    redirect_to root_path
+  end
+
+ private
 
   def pic_params
     params.require(:pic).permit(:title, :description)
   end
 
   def find_pic
-    @pic = Pic.find(params:[:id])
+    @pic = Pic.find(params[:id])
   end
 
 end
